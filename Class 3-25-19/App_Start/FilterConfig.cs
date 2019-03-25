1ed05246401cb8dfc24b85fbca9c861d6ddcd119
Tziri <tzirizohn1@gmail.com>
2019-03-25 19:47:54 -0400

    Add project files.

---
+++ Class 3-25-19/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Class_3_25_19
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Class 3-25-19/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Class_3_25_19.MvcApplication" Language="C#" %>
